8b1591107848935875b78ce7a1bd2ec615023422
bashar35790 <bashar35790@gmail.com>
2026-04-09 20:49:56 +0600

    I've fixed the pricing page mobile humbarger menu make it visible

--- src/components/ui/NavBar/NavBar.jsx
+++ src/components/ui/NavBar/NavBar.jsx
@@ -74,7 +74,7 @@ export default function NavBar() {
           <div className="flex xl:hidden items-center gap-4">
             <button
               type="button"
-              className="text-gray-500 hover:text-gray-700"
+              className={`transition-colors duration-300 ${useWhiteText ? "text-white" : "text-gray-700 hover:text-gray-900"}`}
               onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
             >
               <span className="sr-only">Open main menu</span>
