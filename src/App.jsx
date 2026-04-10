702226f8229c9ee79d97a7b4c2d6ef4af65f3484
ratul-sardar <ratulsardar4747@gmail.com>
2026-04-10 08:32:44 +0600

    added the  / on the route for pricing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import Pricing from "./pages/Pricing";
-import Layout from "./components/layout/Layout"
-
-
+import Layout from "./components/layout/Layout";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="pricing" element={<Pricing />} />
+        <Route path="/pricing" element={<Pricing />} />
       </Route>
     </Routes>
   );
